3e17e318d02c7eabf54d55f915681104d948e11d
jorie21 <visayajorie@gmail.com>
2024-11-03 00:23:28 +0800

    API KEY ADDED

--- configs/firebaseConfig.js
+++ configs/firebaseConfig.js
@@ -5,6 +5,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
     // LAGAY MO LANG MGA CONFIG TAPOS SAVE MO THEN TAPOS NA WALA KANANG GAGAWIN
+  apiKey: "AIzaSyBIeZK9Gg-XjnhT7CL2RMW5rCj-SYEreSM",
+  authDomain: "philosophy-89261.firebaseapp.com",
+  projectId: "philosophy-89261",
+  storageBucket: "philosophy-89261.firebasestorage.app",
+  messagingSenderId: "622614696070",
+  appId: "1:622614696070:web:ebca6d25df5f28d2794107"
 };
 
 // Initialize Firebase
